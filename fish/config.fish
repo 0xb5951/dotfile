bb51127e89b53d089dd6da34052faaaa1e1c6cdf
0xb5951 <oxkeita@gmail.com>
2022-01-01 16:45:36 +0900

    init: fish.configのベースを追加

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# UI系
+
+# PATH系
+fish_add_path /opt/homebrew/bin
+
+# alias
+alias g=git
+alias ls=ls -al
