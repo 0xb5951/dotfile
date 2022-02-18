c80423bc25ece9fa99f28636f214cf19bc576ff4
0xb5951 <oxkeita@gmail.com>
2022-02-18 11:41:19 +0900

    add: pyenv path

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,12 @@ end
 fish_add_path /opt/homebrew/bin
 fish_add_path /Users/mike/development/flutter/bin
 fish_add_path /usr/local/texlive/2021basic/bin
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/mike/google-cloud-sdk/path.fish.inc' ]; . '/Users/mike/google-cloud-sdk/path.fish.inc'; end
+
+# pyenv
+set -x PYENV_ROOT $HOME/.pyenv                                                                                                                         Fri Feb 18 11:39:26 2022
+set -x PATH $PYENV_ROOT/bin $PATH
+status is-login; and pyenv init --path | source
+status is-interactive; and pyenv init - | source
